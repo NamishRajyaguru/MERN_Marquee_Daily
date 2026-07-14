077823a05855f11d7b51c943d552c7c4cf3757db
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-07-14 11:15:52 +0530

    Counter mini project

---
+++ 19_counter/counter.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+function increment() {
+    count++;
+    document.getElementById('count').textContent = count;
+        
+}
+
+function decrement() {
+    count--;
+    document.getElementById('count').textContent = count;
+}
+
+function reset() {
+    count = 0;
+    document.getElementById('count').textContent = count;
+}
